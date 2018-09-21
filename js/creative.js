804e3665cd6e77237ee0c28e0b6c8b57e14997ca
Pei-Qun <nancy016071@gmail.com>
2018-09-21 14:15:26 +0800

    git init

---
+++ js/creative.js
@@ -0,0 +1,31 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 57
+  });
+
+  // Collapse Navbar
+  var navbarCollapse = function() {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
+
+  
+
+  $('#index #shop .con .con-pic .like').click(function(){
+    $(this).toggleClass('click');
+  })
+
+
+})(jQuery); // End of use strict
